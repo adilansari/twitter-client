f16d4fcc5469abd2e5e00260ec419a416b77eead
Adil Ansari <ansari.adil20@gmail.com>
2016-02-19 14:57:50 -0800

    models with find or create method

--- app/src/main/java/com/codepath/apps/twitter/utils/DateConverter.java
+++ app/src/main/java/com/codepath/apps/twitter/utils/DateConverter.java
@@ -1,20 +1,18 @@
 package com.codepath.apps.twitter.utils;
 
+import android.text.format.DateUtils;
 import android.util.Log;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-/**
- * Created by adil on 2/18/16.
- */
 public class DateConverter {
 
     private static SimpleDateFormat sf = new SimpleDateFormat("EEE MMM dd HH:mm:ss ZZZZZ yyyy");
     private static final String TAG = DateConverter.class.getSimpleName();
 
-    public Date setDateFromString(String date) {
+    public static Date getDateFromString(String date) {
         sf.setLenient(true);
         Date d = new Date();
         try {
@@ -24,5 +22,13 @@ public class DateConverter {
             e.printStackTrace();
         }
         return d;
+    }
+
+    public static String getRelativeTimeStamp(Date d) {
+        long dateMillis = d.getTime();
+        String relativeDate = DateUtils.getRelativeTimeSpanString(dateMillis,
+                    System.currentTimeMillis(), DateUtils.SECOND_IN_MILLIS).toString();
+
+        return relativeDate;
     }
 }
