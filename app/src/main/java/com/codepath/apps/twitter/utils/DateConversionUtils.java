da6ff12624d3a335756d896ba5ca454ff985f917
Adil Ansari <ansari.adil20@gmail.com>
2016-02-21 11:51:47 -0800

    Bug with date utils

--- app/src/main/java/com/codepath/apps/twitter/utils/DateConversionUtils.java
+++ app/src/main/java/com/codepath/apps/twitter/utils/DateConversionUtils.java
@@ -11,7 +11,8 @@ import java.util.Date;
 public class DateConversionUtils {
 
     private static SimpleDateFormat sf = new SimpleDateFormat("EEE MMM dd HH:mm:ss ZZZZZ yyyy");
-    private static SimpleDateFormat readableDateFormat = new SimpleDateFormat("yyyy/MM/dd hh:mm a");
+    private static SimpleDateFormat readableDateTimeFormat = new SimpleDateFormat("yyyy/MM/dd hh:mm a");
+    private static SimpleDateFormat readableDateFormat = new SimpleDateFormat("yyyy/MM/dd");
     private static final String TAG = DateConversionUtils.class.getSimpleName();
 
     public static Date getDateFromString(String date) {
@@ -28,23 +29,25 @@ public class DateConversionUtils {
 
     public static String getRelativeTimeStamp(Date d) {
         long time = d.getTime();
-        String relativeTime  = (String) DateUtils.getRelativeTimeSpanString(time, System.currentTimeMillis(), DateUtils.MINUTE_IN_MILLIS, DateUtils.FORMAT_ABBREV_RELATIVE);
+        String relativeTime = (String) DateUtils.getRelativeTimeSpanString(time, System.currentTimeMillis(), DateUtils.MINUTE_IN_MILLIS, DateUtils.FORMAT_ABBREV_RELATIVE);
         String[] tokens = relativeTime.split(" ");
         StringBuilder customRelativeTime = new StringBuilder(tokens[0]);
 
-        if (tokens[1].toLowerCase().contains("mins")){
-            customRelativeTime.append("m");
-        } else if (tokens[1].toLowerCase().contains("hour")){
-            customRelativeTime.append("h");
-        } else if (tokens[1].toLowerCase().contains("day")){
-            customRelativeTime.append("d");
-        } else if (tokens[1].toLowerCase().contains("week")){
-            customRelativeTime.append("w");
+        if (tokens.length > 1) {
+            if (tokens[1].toLowerCase().contains("mins")) customRelativeTime.append("m");
+            else if (tokens[1].toLowerCase().contains("hour")) customRelativeTime.append("h");
+            else if (tokens[1].toLowerCase().contains("day")) customRelativeTime.append("d");
+            else if (tokens[1].toLowerCase().contains("week")) customRelativeTime.append("w");
         }
+
         return customRelativeTime.toString();
     }
 
-    public static String getReadableDate(Date d){
+    public static String getReadableDateTime(Date d) {
+        return readableDateTimeFormat.format(d);
+    }
+
+    public static String getReadableDate(Date d) {
         return readableDateFormat.format(d);
     }
 }
