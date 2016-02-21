5dfcda763e0550144c44ba5fb7d60578bc0c2017
Adil Ansari <ansari.adil20@gmail.com>
2016-02-21 11:25:55 -0800

    Linkified text view

--- app/src/main/java/com/codepath/apps/twitter/activities/TweetDetailActivity.java
+++ app/src/main/java/com/codepath/apps/twitter/activities/TweetDetailActivity.java
@@ -3,7 +3,6 @@ package com.codepath.apps.twitter.activities;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.ViewStub;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -25,7 +24,9 @@ public class TweetDetailActivity  extends AppCompatActivity{
     @Bind(R.id.detail_tvTweetText) TextView tvTweetText;
     @Bind(R.id.detail_stubMedia) ViewStub mediaStub;
     @Bind(R.id.detail_tvTimestamp) TextView tvTimestamp;
-    @Bind(R.id.detail_btnReply) Button btnReply;
+    @Bind(R.id.detail_ivReplyToTweetIcon) ImageView btnReply;
+    @Bind(R.id.detail_tvRetweetCount) TextView tvRetweetCount;
+    @Bind(R.id.detail_tvFavoriteCount) TextView tvFavoriteCount;
 
     private Tweet tweet;
 
@@ -45,5 +46,7 @@ public class TweetDetailActivity  extends AppCompatActivity{
         tvScrenName.setText(TextConversionUtils.screenName(tweet.user.screenName));
         tvTweetText.setText(TextConversionUtils.linkify(tweet.text));
         tvTimestamp.setText(tweet.getReadableDate());
+        tvRetweetCount.setText(Integer.toString(tweet.retweetCount));
+        tvFavoriteCount.setText(Integer.toString(tweet.favoriteCount));
     }
 }
