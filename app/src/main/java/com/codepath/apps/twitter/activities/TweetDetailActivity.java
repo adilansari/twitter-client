bd5b2642aed317f3adb5cbcbc5783f4f44db08ab
Adil Ansari <ansari.adil20@gmail.com>
2016-02-28 19:54:36 -0800

    actions from detail page

--- app/src/main/java/com/codepath/apps/twitter/activities/TweetDetailActivity.java
+++ app/src/main/java/com/codepath/apps/twitter/activities/TweetDetailActivity.java
@@ -5,6 +5,8 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
 import android.view.ViewStub;
 import android.widget.ImageView;
 import android.widget.MediaController;
@@ -13,9 +15,14 @@ import android.widget.VideoView;
 
 import com.bumptech.glide.Glide;
 import com.codepath.apps.twitter.R;
+import com.codepath.apps.twitter.TwitterApplication;
+import com.codepath.apps.twitter.TwitterClient;
 import com.codepath.apps.twitter.models.Tweet;
 import com.codepath.apps.twitter.utils.TextConversionUtils;
+import com.loopj.android.http.JsonHttpResponseHandler;
 
+import org.apache.http.Header;
+import org.json.JSONObject;
 import org.parceler.Parcels;
 
 import butterknife.Bind;
@@ -32,11 +39,14 @@ public class TweetDetailActivity extends AppCompatActivity {
     @Bind(R.id.detail_ivReplyToTweetIcon) ImageView btnReply;
     @Bind(R.id.detail_tvRetweetCount) TextView tvRetweetCount;
     @Bind(R.id.detail_tvFavoriteCount) TextView tvFavoriteCount;
+    @Bind(R.id.detail_ivRetweetIcon) ImageView ivRetweetIcon;
+    @Bind(R.id.detail_ivFavoriteIcon) ImageView ivFavoriteIcon;
 
     private Tweet tweet;
     private MediaController mediaController;
     private Uri videoUri;
     private Toolbar toolbar;
+    private TwitterClient mClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,6 +69,47 @@ public class TweetDetailActivity extends AppCompatActivity {
         tvTimestamp.setText(tweet.getReadableDate());
         tvRetweetCount.setText(Integer.toString(tweet.retweetCount));
         tvFavoriteCount.setText(Integer.toString(tweet.favoriteCount));
+
+        if (tweet.retweeted) ivRetweetIcon.setImageResource(R.drawable.ic_retweet_action_active);
+        if (tweet.favorited) ivFavoriteIcon.setImageResource(R.drawable.ic_like_action_active);
+
+        final String tweetId = String.valueOf(tweet.tweetId);
+        ivRetweetIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mClient = TwitterApplication.getTwitterClient();
+                Log.d("action", tweetId);
+                mClient.postRetweet(tweetId, new JsonHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                        ivRetweetIcon.setImageResource(R.drawable.ic_retweet_action_active);
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                        Log.e("action", errorResponse.toString());
+                    }
+                });
+            }
+        });
+
+        ivFavoriteIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mClient = TwitterApplication.getTwitterClient();
+                mClient.postFavorite(tweetId, new JsonHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                        ivFavoriteIcon.setImageResource(R.drawable.ic_like_action_active);
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                        Log.e("action", errorResponse.toString());
+                    }
+                });
+            }
+        });
 
         if (tweet.isVideoTweet()) {
             mediaStub.setLayoutResource(R.layout.detail_tweet_video);
